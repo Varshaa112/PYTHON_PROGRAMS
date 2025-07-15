71cb7e7e5e9345ef9b9082c5248a2442cedcfc48
Varshaa112 <142001487+Varshaa112@users.noreply.github.com>
2025-07-15 21:19:01 +0530

    Add files via upload

---
+++ LEETCODE367.py
@@ -0,0 +1,19 @@
+#iven a positive integer num, return true if num is a perfect square or false otherwise.
+class Solution(object):
+    def isPerfectSquare(self, num):
+        if num < 2:
+            return True
+
+        left, right = 2, num // 2
+        while left <= right:
+            mid = (left + right) // 2
+            squared = mid * mid
+            if squared == num:
+                return True
+            elif squared < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
+
