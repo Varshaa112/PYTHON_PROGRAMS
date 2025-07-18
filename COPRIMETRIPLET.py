aa90a6f59fade8c64716c5bcd3a12d270ce24577
Varshaa112 <142001487+Varshaa112@users.noreply.github.com>
2025-07-18 11:58:42 +0530

    Add files via upload

---
+++ COPRIMETRIPLET.py
@@ -0,0 +1,21 @@
+num=int(input("enter number"))
+
+def gcd(a,b): #type int
+    while b!=0:
+        temp=a%b
+        a=b
+        b=temp
+    return a
+
+def cop(a,b): #type boolean
+    return gcd(a,b)==1
+
+for i in range(1,num):
+    for j in range(1,i):
+        for k in range(1,j):
+            if j*j + k*k == i*i and cop(i,j) and cop(j,k) and cop(i,k):
+                print(k,j,i)
+
+
+
+
